651846fe4a775ebaf7f30099be17cdc494c1d13e
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2024-11-12 14:05:27 +0100

    more things working

--- Programming/Android Projects/2024-11-12/app/src/main/java/com/example/loginlayout/LoginActivity.java
+++ Programming/Android Projects/2024-11-12/app/src/main/java/com/example/loginlayout/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginlayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,39 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button loginButton;
+    Button registerButton;
+
+    TextView passwordLogin;
+    TextView emailLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.dashboard), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loginButton = findViewById(R.id.loginButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        passwordLogin = findViewById(R.id.passwordLogin);
+        emailLogin = findViewById(R.id.emailLogin);
+
+
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegistrationActivity.class);
+            startActivity(intent);
+        });
+
+        loginButton.setOnClickListener(v -> {
+                    Intent intent = new Intent(this, DashboardActivity.class);
+                    startActivity(intent);
+                }
+        );
     }
 }
