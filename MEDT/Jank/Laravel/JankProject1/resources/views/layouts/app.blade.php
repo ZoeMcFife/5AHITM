989580b4079d75bdff724be9c0706c8053aeed9e
Zoe McFife <93198038+ZoeMcFife@users.noreply.github.com>
2025-02-28 11:44:24 +0100

    asdada

--- MEDT/Jank/Laravel/JankProject1/resources/views/layouts/app.blade.php
+++ MEDT/Jank/Laravel/JankProject1/resources/views/layouts/app.blade.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'My Laravel Site')</title>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- DataTables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
     <style>
         body {
             font-family: Arial, sans-serif;
@@ -52,5 +62,12 @@
 <footer>
     <a href="/impressum">Impressum</a> | &copy; {{ date('Y-M-d') }}
 </footer>
+
+<!-- Bootstrap JS -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+<!-- DataTables JS -->
+<script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
 </body>
 </html>
